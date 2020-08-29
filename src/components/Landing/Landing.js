9c0c31e3f5554d27d51846748347a59f14a7809d
Quinn Elder <qre.turing@gmail.com>
2020-08-29 13:39:07 -0600

    Start editing Landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,10 +1,53 @@
 import React from "react";
+import styled from "styled-components";
+
+const MainLandingContainer = styled.div`
+  width: 50em;
+  margin: 0 auto;
+`;
+
+const Header = styled.section`
+  padding: 20px;
+	margin-bottom: 40px;
+	background-color: #fff;
+	@include border-radius(4px);
+	@include box-shadow(0 1px 0 rgba(0,0,0,.2));
+`;
+
+const HeaderTitle = styled.h2`
+  font-size: 34px;
+	text-transform: uppercase;
+	font-weight: 100;
+	line-height: 2;
+	color: #2c3e50;
+`;
+
+const HeaderParagraph = styled.p`
+  margin-bottom: 2rem;
+	line-height: 2;
+	color: #7f8c8d; 
+`;
 
 const Landing = () => {
   return (
-    <div>
-      <h1>HELLOOOO</h1>
-    </div>
+    <MainLandingContainer>
+
+      <Header>
+        <HeaderTitle>About this project</HeaderTitle>
+        <HeaderParagraph>about section</HeaderParagraph>
+      </Header>
+
+      <Header>
+        <HeaderTitle>project walk through</HeaderTitle>
+        <HeaderParagraph>walk through </HeaderParagraph>
+      </Header>
+
+      <Header>
+        <HeaderTitle>development team</HeaderTitle>
+        <HeaderParagraph>about the team</HeaderParagraph>
+      </Header>
+
+    </MainLandingContainer>
   );
 };
 
