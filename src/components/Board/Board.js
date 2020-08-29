a29a4505a05cbeb1fb4efa1d16825a484744c05c
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-08-28 22:06:59 -0600

    Render out a Column component in the Board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import initialData from "../../initial-data";
+import Column from "../Column/Column";
 
-const Board = () => {
-  return <div>Add board here</div>;
-};
+class Board extends Component {
+  state = initialData;
+
+  render() {
+    return this.state.columnOrder.map((columnId) => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    });
+  }
+}
 
 export default Board;
