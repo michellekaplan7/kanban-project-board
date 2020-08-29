6c27340852c3c81694cad2dce8f46eefe92191e0
Quinn Elder <qre.turing@gmail.com>
2020-08-29 17:24:39 -0600

    Change pops

--- src/components/InnerColumnList/InnerColumnList.js
+++ src/components/InnerColumnList/InnerColumnList.js
@@ -3,9 +3,9 @@ import Column from "../Column/Column";
 
 class InnerColumnList extends PureComponent {
   render() {
-    const { column, taskMap, index } = this.props;
+    const { column, taskMap, index, addNewTaskToColumnList } = this.props;
     const tasks = column.taskIds.map((taskId) => taskMap[taskId]);
-    return <Column column={column} tasks={tasks} index={index} />;
+    return <Column column={column} tasks={tasks} index={index} addNewTaskToColumnList={addNewTaskToColumnList} />;
   }
 }
 
