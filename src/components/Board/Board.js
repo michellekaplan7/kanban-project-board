d8ee773516cd14120095b6091f544531d05ab622
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-08-28 22:57:01 -0600

    Tasks display on board and can move them around

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import initialData from "../../initial-data";
 import Column from "../Column/Column";
+import { DragDropContext } from "react-beautiful-dnd";
 
 class Board extends Component {
   state = initialData;
 
+  onDragEnd = result => {
+    // TODO: reorder our column
+
+  }
+
   render() {
-    return this.state.columnOrder.map((columnId) => {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+      {this.state.columnOrder.map(columnId => {
       const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
       return <Column key={column.id} column={column} tasks={tasks} />;
-    });
+      })}
+      </DragDropContext>
+    );
   }
 }
 
