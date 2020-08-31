575fa52e7c2fc7eebbe6f28e7a2b4a172514dfe6
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-08-30 22:28:18 -0600

    Updates

---
+++ precache-manifest.1560b33354ef053b039f4f3328f7aa10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f9d3fb984a51ee4c84cc20a08a74fb",
+    "url": "/kanban-project-board/index.html"
+  },
+  {
+    "revision": "3ef29cd0a30d1e56643d",
+    "url": "/kanban-project-board/static/css/2.3107b260.chunk.css"
+  },
+  {
+    "revision": "b90f82bf5343176b495c",
+    "url": "/kanban-project-board/static/css/main.a89e4185.chunk.css"
+  },
+  {
+    "revision": "3ef29cd0a30d1e56643d",
+    "url": "/kanban-project-board/static/js/2.c29b4309.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/kanban-project-board/static/js/2.c29b4309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90f82bf5343176b495c",
+    "url": "/kanban-project-board/static/js/main.0c5ef821.chunk.js"
+  },
+  {
+    "revision": "40ddda6c25cb71f87420",
+    "url": "/kanban-project-board/static/js/runtime-main.f1649f72.js"
+  }
+]);
